8332d34298989c17f086e6e6b5c52e6dee456511
pinheiro27 <silvadriver1982@gmail.com>
2025-01-25 12:26:08 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistop/DslistopApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistop/DslistopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
